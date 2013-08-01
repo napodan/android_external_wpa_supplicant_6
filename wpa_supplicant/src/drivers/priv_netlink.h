22b0b0c78221f06649f62da57005efa0fcc7181b
napodan <david.guenet@gmail.com>
2013-08-01 13:18:36 +0200

    Update to use thalamus kernel

--- wpa_supplicant/src/drivers/priv_netlink.h
+++ wpa_supplicant/src/drivers/priv_netlink.h
@@ -51,11 +51,6 @@
 #define RTM_DELLINK (RTM_BASE + 1)
 #define RTM_SETLINK (RTM_BASE + 3)
 
-#define NLMSG_ALIGNTO 4
-#define NLMSG_ALIGN(len) (((len) + NLMSG_ALIGNTO - 1) & ~(NLMSG_ALIGNTO - 1))
-#define NLMSG_LENGTH(len) ((len) + NLMSG_ALIGN(sizeof(struct nlmsghdr)))
-#define NLMSG_DATA(nlh) ((void*) (((char*) nlh) + NLMSG_LENGTH(0)))
-
 #define RTA_ALIGNTO 4
 #define RTA_ALIGN(len) (((len) + RTA_ALIGNTO - 1) & ~(RTA_ALIGNTO - 1))
 #define RTA_OK(rta,len) \
@@ -67,23 +62,6 @@
 #define RTA_LENGTH(len) (RTA_ALIGN(sizeof(struct rtattr)) + (len))
 #define RTA_DATA(rta) ((void *) (((char *) (rta)) + RTA_LENGTH(0)))
 
-
-struct sockaddr_nl
-{
-	sa_family_t nl_family;
-	unsigned short nl_pad;
-	u32 nl_pid;
-	u32 nl_groups;
-};
-
-struct nlmsghdr
-{
-	u32 nlmsg_len;
-	u16 nlmsg_type;
-	u16 nlmsg_flags;
-	u32 nlmsg_seq;
-	u32 nlmsg_pid;
-};
 
 struct ifinfomsg
 {
